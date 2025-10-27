e14ae7dd17d49c1ab8fad5d5d6df0a5d0f6695b7
Nicolas DURET <45012628+NicolasDuret@users.noreply.github.com>
2025-10-27 14:31:32 +0100

    FixCaroussel

--- js/work.js
+++ js/work.js
@@ -1,9 +1,18 @@
-// Dupliquer les projets pour créer une boucle infinie
-const track = document.querySelector(".carousel-track");
-const projects = Array.from(track.children);
-
-// Cloner les projets pour un effet loop
-projects.forEach((proj) => {
-  const clone = proj.cloneNode(true);
-  track.appendChild(clone);
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide("#image-carousel", {
+    type: "loop",
+    perPage: 3, // 3 images visibles
+    perMove: 1,
+    autoplay: true,
+    interval: 2000, // 3s par image
+    pauseOnHover: true, // pause quand on survole
+    arrows: false, // flèches navigation
+    pagination: false, // pas de petits points
+    gap: "20px", // espace entre images
+    breakpoints: {
+      1024: { perPage: 2 },
+      768: { perPage: 1 },
+    },
+  });
+  splide.mount();
 });
